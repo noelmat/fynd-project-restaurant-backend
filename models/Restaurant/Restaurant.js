68df6c238f468e3073c000a93896b664f82d8234
Noel Mathew <noel.mathew007@gmail.com>
2021-06-18 01:45:43 +0530

    added Restaurant Model

---
+++ models/Restaurant/Restaurant.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const timeSchema = new mongoose.Schema({
+    hours: {
+        type: Number,
+        min: 0,
+        max: 23,
+        required: true
+    },
+    minutes: {
+        type: Number,
+        min: 0,
+        max: 59,
+        required: true
+    }
+})
+
+const restaurantSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: mongoose.Schema.Types.ObjectId,
+    },
+    openTime: {
+        type: timeSchema,
+        required: true,
+        _id: false,
+    },
+    closeTime: {
+        type: timeSchema,
+        required: true,
+        _id: false,
+    },
+    status: {
+        type: String,
+        enum: ['open', 'closed'],
+        default: 'closed'
+    }
+});
+
+mongoose.model('Restaurant', restaurantSchema);
