50bb6ad551ae1fb5daa9ffe08eda9059f34a2440
Noel Mathew <noel.mathew007@gmail.com>
2021-06-18 16:48:35 +0530

    feat: Added Auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const token = req.header('Authorization');
+
+    if (!token) {
+        const error = new Error('Provide Authentication');
+        error.status = 401;
+        next(error);
+    }
+
+    jwt.verify(token, 'SECRET', (err, claims) => {
+        if (err) {
+            const error = new Error('Provide Valid Authentication');
+            error.status = 401;
+            return next(error);
+        }
+
+        res.locals.claims = claims;
+        next();
+    })
+}
+
+const authorize = (req, res, next) => {
+    const claims = res.locals.claims;
+
+    if (claims.role !== 'admin') {
+        const error = new Error('Unauthorized Access');
+        error.status = 403;
+        return next(error);
+    }
+
+    next();
+}
+
+module.exports = {
+    authenticate, 
+    authorize
+}
