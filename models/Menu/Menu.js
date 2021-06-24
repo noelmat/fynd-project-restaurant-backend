f290c788f39795ce8f35c99df0abbcbffc3f947e
Noel Mathew <noel.mathew007@gmail.com>
2021-06-24 21:14:41 +0530

    fix: updated Menu Schema

--- models/Menu/Menu.js
+++ models/Menu/Menu.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const timeSchema = require('../TimeSchema');
 
 const menuSchema = new mongoose.Schema({
     name: {
@@ -7,10 +8,28 @@ const menuSchema = new mongoose.Schema({
         unique: true
     },
     menuItems: {
-        type: [ mongoose.Schema.Types.ObjectId ],
+        type: [ {type: mongoose.Schema.Types.ObjectId, ref:'MenuItem'} ],
         default: []
+    },
+    availability: {
+        type: [Boolean],
+        default: [true, true, true, true, true, true, true],
+        validate: [val=> val.length === 7, 'Input for 7 days required.'],
+    },
+    openTime : {
+        type: timeSchema, 
+        required: true,
+        _id: false
+    },
+    closeTime : {
+        type: timeSchema, 
+        required: true,
+        _id: false
     }
-
 })
 
+
+// menuSchema.pre('findOneAndDelete', function(){
+//     console.log(this);
+// })
 mongoose.model('Menu', menuSchema);
