949cc38cf45c2959b1c8697974b45e0654451817
Noel Mathew <noel.mathew007@gmail.com>
2021-06-24 21:15:33 +0530

    feat: added support for menu and menuitem

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,31 +1,22 @@
 const express = require('express');
 const { authenticate, authorize } = require('../middlewares/auth');
 const { registerRestaurant } = require('../controllers/restaurantManagement/restaurant');
-const { addMenu, updateMenu, deleteMenu} = require('../controllers/restaurantManagement/menu');
+const { getMenus, getMenu, addMenu, updateMenu, deleteMenu} = require('../controllers/restaurantManagement/menu');
+const { addMenuItem, updateMenuItem, deleteMenuItem} = require('../controllers/restaurantManagement/menuItem');
 const router = express.Router();
 
 router.post('/',authenticate, authorize, registerRestaurant);
-/**
- *  Menu will have a restaurant ID
- *  1. Create Menu
- *  2. Update Menu
- *  3. Delete Menu
- * 
- */
+
+router.get('/menu', getMenus);
+router.get('/menu/:id', getMenu);
 router.post('/menu', authenticate, authorize, addMenu);
 router.patch('/menu/:id', authenticate, authorize, updateMenu);
 router.delete('/menu/:id', authenticate, authorize, deleteMenu);
 
 
-/**
- *  MenuItem will have a restaurant ID
- *  1. Create MenuItem
- *  2. Update MenuItem
- *  3. Delete MenuItem
- */
-// router.post('/menuitem', authenticate, authorize, addMenuItem);
-// router.patch('/menuitem/:id', authenticate, authorize, updateMenuItem);
-// router.delete('/menuitem/:id', authenticate, authorize, deleteMenuItem);
+router.post('/menuitem', authenticate, authorize, addMenuItem);
+router.patch('/menuitem/:id', authenticate, authorize, updateMenuItem);
+router.delete('/menuitem/:id', authenticate, authorize, deleteMenuItem);
 
 
 
