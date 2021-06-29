b475c819fbb2316ba512b2c85ddc6e5065c5868d
Noel Mathew <noel.mathew007@gmail.com>
2021-06-29 23:33:38 +0530

    Seed Data

---
+++ data/seed.js
@@ -0,0 +1,13 @@
+const { execSync } = require('child_process');
+
+const DB_NAME = 'restaurantsDB';
+
+try {
+    execSync(`mongoimport --db ${DB_NAME} --collection addresses --drop --file "${process.cwd()}/data/seed/addresses.json" --jsonArray`);
+    execSync(`mongoimport --db ${DB_NAME} --collection restaurants --drop --file "${process.cwd()}/data/seed/restaurants.json" --jsonArray`);
+    execSync(`mongoimport --db ${DB_NAME} --collection restaurantuser --drop --file "${process.cwd()}/data/seed/restaurantusers.json" --jsonArray`);
+    console.log(`Imported documents into database ${DB_NAME}`);
+}catch(err){
+    console.log( `Could not import documents into database ${DB_NAME}`);
+    console.log( err );
+}
